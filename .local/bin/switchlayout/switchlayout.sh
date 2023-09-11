50146aefc9ed1604407869ca52ddec5d7e3ff275
spocksbeerd <albueduard21@gmail.com>
2023-09-11 20:30:28 +0300

    feat: added a more appealing look

--- .local/bin/switchlayout/switchlayout.sh
+++ .local/bin/switchlayout/switchlayout.sh
@@ -5,12 +5,10 @@ current_layout=$(cat $scriptdir/currentlayout)
 
 if [ "$current_layout" = "focused" ]; then
     cp -r $scriptdir/i3pretty ~/.config/i3/appearance
-    cp -r $scriptdir/polybarpretty ~/.config/polybar/border.ini
     picom -b &
     echo "pretty" > $scriptdir/currentlayout
 else
     cp -r $scriptdir/i3focused ~/.config/i3/appearance
-    cp -r $scriptdir/polybarfocused ~/.config/polybar/border.ini
     pkill picom
     echo "focused" > $scriptdir/currentlayout
 fi
