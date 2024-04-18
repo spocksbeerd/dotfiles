a56d57c103dc8cdd6911a704f5d8966ecbd5443c
spocksbeerd <albueduard21@gmail.com>
2024-04-18 23:15:13 +0300

    added switchlayout for bspwm

--- .local/bin/switchlayout.sh
+++ .local/bin/switchlayout.sh
@@ -5,10 +5,18 @@ current_layout=$(cat $layout_dir/currentlayout)
 
 if [ "$current_layout" = "focused" ]; then
     cp -r $layout_dir/i3pretty ~/.config/i3/appearance
+
+    bspc config window_gap 8
+    bspc config borderless_monocle false
+
     picom -b &
     echo "pretty" > $layout_dir/currentlayout
 else
     cp -r $layout_dir/i3focused ~/.config/i3/appearance
+
+    bspc config window_gap 0
+    bspc config borderless_monocle true
+
     pkill picom
     echo "focused" > $layout_dir/currentlayout
 fi
