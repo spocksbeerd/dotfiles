5ae61329c94f22af5f3ea7a7c9d2bcb3247c985d
spocksbeerd <albueduard21@gmail.com>
2024-05-12 16:56:25 +0300

    Routine changes

--- .local/bin/switchlayout.sh
+++ .local/bin/switchlayout.sh
@@ -7,20 +7,16 @@ if [ "$current_layout" = "focused" ]; then
     cp -r $layout_dir/alacritty_pretty.toml ~/.config/alacritty/window.toml
     cp -r $layout_dir/polybar_pretty.ini ~/.config/polybar/colors.ini
     cp -r $layout_dir/picom_pretty ~/.config/picom/picom.conf
-
-    bspc config window_gap 8
-    bspc config borderless_monocle false
+    cp -r $layout_dir/bspwm_pretty ~/.config/bspwm/appearance
 
     echo "pretty" > $layout_dir/currentlayout
 else
     cp -r $layout_dir/alacritty_focused.toml ~/.config/alacritty/window.toml
     cp -r $layout_dir/polybar_focused.ini ~/.config/polybar/colors.ini
     cp -r $layout_dir/picom_focused ~/.config/picom/picom.conf
-
-    bspc config window_gap 0
-    bspc config borderless_monocle true
+    cp -r $layout_dir/bspwm_focused ~/.config/bspwm/appearance
 
     echo "focused" > $layout_dir/currentlayout
 fi
 
-polybar.sh &
+bspc wm -r
